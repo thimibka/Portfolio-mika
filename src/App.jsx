8bf629883511bd019c5d00443aa4f23c4885739b
Mickael <fseetp@gmail.com>
2024-01-04 00:15:07 +0100

    change cards boot

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "../src/styles/all.css";
 import { Routes, Route } from "react-router-dom";
 import Presentation from "./Pages/Presentation";
 import Formation from "./Pages/Formation";
 import Diplomes from "./Pages/Diplomes";
 import Projets from "./Pages/Projets";
-
 import NavPortFolio from "./Components/Navbar";
 
 function App() {
