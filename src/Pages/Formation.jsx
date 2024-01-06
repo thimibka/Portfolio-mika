02166f8dc10370966736e636d154528963ca15a9
Mickael <fseetp@gmail.com>
2024-01-06 17:02:38 +0100

    center card

--- src/Pages/Formation.jsx
+++ src/Pages/Formation.jsx
@@ -1,62 +1,74 @@
+import Card from "react-bootstrap/Card";
 export default function Formation() {
   return (
     <>
       <h1>Formation THP</h1>
 
-      <p>
-        Formation en perr-learning de 1250h ou nous avons appris les sujet
-        suivant :<br />
-        -Terminal
-        <br />
-        -Html
-        <br />
-        -Css
-        <br />
-        -Git hub
-        <br />
-        -Bootstrap
-        <br />
-        -Atomic design
-        <br />
-        -Ruby
-        <br />
-        -Programation orienté objet
-        <br />
-        -Model vue controller
-        <br />
-        -Rails
-        <br />
-        -SQL POSTGRESQL
-        <br />
-        -Deployement fly.io/Heroku
-        <br />
-        -Javascript vanilla
-        <br />
-        -Dom
-        <br />
-        -Figma
-        <br />
-        -Trello
-        <br />
-        -Méthode agile
-        <br />
-        -ES6 variables,fonction et methodes
-        <br />
-        -ES6 les classes
-        <br />
-        -La récursivité
-        <br />
-        -La complexité algorithmique
-        <br />
-        -Les arbres binaires
-        <br />
-        -Regex
-        <br />
-        -Api
-        <br />
-        -React.js
-        <br />
-      </p>
+      <div className="formation">
+        <Card style={{ width: "30rem" }}>
+          <Card.Body>
+            <Card.Title>
+              Formation en perr-learning de 1250h ou nous avons appris les sujet
+              suivant :
+            </Card.Title>
+            <Card.Text>
+              <p>
+                <br />
+                -Terminal
+                <br />
+                -Html
+                <br />
+                -Css
+                <br />
+                -Git hub
+                <br />
+                -Bootstrap
+                <br />
+                -Atomic design
+                <br />
+                -Ruby
+                <br />
+                -Programation orienté objet
+                <br />
+                -Model vue controller
+                <br />
+                -Rails
+                <br />
+                -SQL POSTGRESQL
+                <br />
+                -Deployement fly.io/Heroku
+                <br />
+                -Javascript vanilla
+                <br />
+                -Dom
+                <br />
+                -Figma
+                <br />
+                -Trello
+                <br />
+                -Méthode agile
+                <br />
+                -ES6 variables,fonction et methodes
+                <br />
+                -ES6 les classes
+                <br />
+                -La récursivité
+                <br />
+                -La complexité algorithmique
+                <br />
+                -Les arbres binaires
+                <br />
+                -Regex
+                <br />
+                -Api
+                <br />
+                -React.js
+                <br />
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 }
--- src/Pages/Projets.jsx
+++ src/Pages/Projets.jsx
@@ -4,74 +4,75 @@ export default function Projets() {
   return (
     <>
       <h1>Projets</h1>
+      <div className="projets">
+        <ul>
+          <Card style={{ width: "50rem" }}>
+            <Card.Img variant="top" src="src/assets/projets/image1.jpg" />
+            <Card.Body>
+              <Card.Title>Projet parcours full stack :</Card.Title>
+              <Card.Text>
+                <p>
+                  Creation d&apos;un site pour un patron de plusieurs complexes
+                  d&apos;excalade.
+                  <br />
+                  Pour la parti client, l&apos;utilisateur poura voir tout les
+                  service du complexe (restauration, coworking,loisir,detente)
+                  et aussi acheter un abonnement, ou reserver un cours.
+                </p>{" "}
+                <p>Pour voir le projet :</p>
+              </Card.Text>
+              <Button
+                a
+                href=" https://theclimbingproject.herokuapp.com/"
+                variant="primary"
+              >
+                Click ici
+              </Button>
+            </Card.Body>
+          </Card>
 
-      <ul>
-        <Card style={{ width: "50rem" }}>
-          <Card.Img variant="top" src="src/assets/projets/image1.jpg" />
-          <Card.Body>
-            <Card.Title>Projet parcours full stack :</Card.Title>
-            <Card.Text>
-              <p>
-                Creation d&apos;un site pour un patron de plusieurs complexes
-                d&apos;excalade.
-                <br />
-                Pour la parti client, l&apos;utilisateur poura voir tout les
-                service du complexe (restauration, coworking,loisir,detente) et
-                aussi acheter un abonnement, ou reserver un cours.
-              </p>{" "}
-              <p>Pour voir le projet :</p>
-            </Card.Text>
-            <Button
-              a
-              href=" https://theclimbingproject.herokuapp.com/"
-              variant="primary"
-            >
-              Click ici
-            </Button>
-          </Card.Body>
-        </Card>
+          <Card style={{ width: "50rem" }}>
+            <Card.Img variant="top" src="src/assets/projets/image2.jpg" />
+            <Card.Body>
+              <Card.Title> Projet parcours dev :</Card.Title>
+              <Card.Text>
+                <p>
+                  Création d&apos;un site de blind test. Possibilité de
+                  s&apos;inscrire, donnant droit à un suivi de point et de
+                  nombre de parti jouées.{" "}
+                </p>
+              </Card.Text>
+              <Button
+                a
+                href="https://musicdetective.vercel.app/"
+                variant="primary"
+              >
+                Click ici
+              </Button>
+            </Card.Body>
+          </Card>
 
-        <Card style={{ width: "50rem" }}>
-          <Card.Img variant="top" src="src/assets/projets/image2.jpg" />
-          <Card.Body>
-            <Card.Title> Projet parcours dev :</Card.Title>
-            <Card.Text>
-              <p>
-                Création d&apos;un site de blind test. Possibilité de
-                s&apos;inscrire, donnant droit à un suivi de point et de nombre
-                de parti jouées.{" "}
-              </p>
-            </Card.Text>
-            <Button
-              a
-              href="https://musicdetective.vercel.app/"
-              variant="primary"
-            >
-              Click ici
-            </Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: "50rem" }}>
-          <Card.Img variant="top" src="src/assets/projets/image3.jpg" />
-          <Card.Body>
-            <Card.Title> Projet perso</Card.Title>
-            <Card.Text>
-              <p>
-                Creation d&apos;un site vitrine presentant toute les creations
-                faites au crochet
-              </p>
-            </Card.Text>
-            <Button
-              a
-              href="https://crochets-de-marinette.vercel.app/"
-              variant="primary"
-            >
-              Click ici
-            </Button>
-          </Card.Body>
-        </Card>
-      </ul>
+          <Card style={{ width: "50rem" }}>
+            <Card.Img variant="top" src="src/assets/projets/image3.jpg" />
+            <Card.Body>
+              <Card.Title> Projet perso</Card.Title>
+              <Card.Text>
+                <p>
+                  Creation d&apos;un site vitrine presentant toute les creations
+                  faites au crochet
+                </p>
+              </Card.Text>
+              <Button
+                a
+                href="https://crochets-de-marinette.vercel.app/"
+                variant="primary"
+              >
+                Click ici
+              </Button>
+            </Card.Body>
+          </Card>
+        </ul>
+      </div>
     </>
   );
 }
