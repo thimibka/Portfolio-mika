de16f4f512b0e1924e258c9da50d4ba92ae861c6
Mickael <fseetp@gmail.com>
2024-01-07 14:28:51 +0100

    add svg projets2

--- src/Pages/Projets.jsx
+++ src/Pages/Projets.jsx
@@ -1,81 +1,3 @@
-// import Button from "react-bootstrap/Button";
-// import Card from "react-bootstrap/Card";
-// export default function Projets() {
-//   return (
-//     <>
-//       <h1>Projets</h1>
-//       <div className="projets">
-//         <ul>
-//           <Card style={{ width: "50rem" }}>
-//             <Card.Img variant="top" src="/projets/image1.jpg" />
-//             <Card.Body>
-//               <Card.Title>Projet parcours full stack :</Card.Title>
-//               <Card.Text>
-//                 <p>
-//                   Creation d&apos;un site pour un patron de plusieurs complexes
-//                   d&apos;excalade.
-//                   <br />
-//                   Pour la parti client, l&apos;utilisateur poura voir tout les
-//                   service du complexe (restauration, coworking,loisir,detente)
-//                   et aussi acheter un abonnement, ou reserver un cours.
-//                 </p>{" "}
-//                 <p>Pour voir le projet :</p>
-//               </Card.Text>
-//               <Button
-//                 a
-//                 href=" https://theclimbingproject.herokuapp.com/"
-//                 variant="primary"
-//               >
-//                 Click ici
-//               </Button>
-//             </Card.Body>
-//           </Card>
-
-//           <Card style={{ width: "50rem" }}>
-//             <Card.Img variant="top" src="/projets/image2.jpg" />
-//             <Card.Body>
-//               <Card.Title> Projet parcours dev :</Card.Title>
-//               <Card.Text>
-//                 <p>
-//                   Création d&apos;un site de blind test. Possibilité de
-//                   s&apos;inscrire, donnant droit à un suivi de point et de
-//                   nombre de parti jouées.{" "}
-//                 </p>
-//               </Card.Text>
-//               <Button
-//                 a
-//                 href="https://musicdetective.vercel.app/"
-//                 variant="primary"
-//               >
-//                 Click ici
-//               </Button>
-//             </Card.Body>
-//           </Card>
-
-//           <Card style={{ width: "50rem" }}>
-//             <Card.Img variant="top" src="/projets/image3.jpg" />
-//             <Card.Body>
-//               <Card.Title> Projet perso</Card.Title>
-//               <Card.Text>
-//                 <p>
-//                   Creation d&apos;un site vitrine presentant toute les creations
-//                   faites au crochet
-//                 </p>
-//               </Card.Text>
-//               <Button
-//                 a
-//                 href="https://crochets-de-marinette.vercel.app/"
-//                 variant="primary"
-//               >
-//                 Click ici
-//               </Button>
-//             </Card.Body>
-//           </Card>
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
@@ -88,7 +10,7 @@ export default function Projets() {
       <div className="projets">
         <Row xs={1} sm={2} md={3} lg={3} xl={3}>
           <Col>
-            <Card style={{ width: "100%" }}>
+            <Card style={{ width: "80%" }}>
               <Card.Img variant="top" src="/projets/image1.jpg" />
               <Card.Body>
                 <Card.Title>Projet parcours full stack :</Card.Title>
@@ -115,7 +37,7 @@ export default function Projets() {
           </Col>
 
           <Col>
-            <Card style={{ width: "100%" }}>
+            <Card style={{ width: "80%" }}>
               <Card.Img variant="top" src="/projets/image2.jpg" />
               <Card.Body>
                 <Card.Title> Projet parcours dev :</Card.Title>
@@ -137,7 +59,7 @@ export default function Projets() {
           </Col>
 
           <Col>
-            <Card style={{ width: "100%" }}>
+            <Card style={{ width: "80%" }}>
               <Card.Img variant="top" src="/projets/image3.jpg" />
               <Card.Body>
                 <Card.Title> Projet perso</Card.Title>
