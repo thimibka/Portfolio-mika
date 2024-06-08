a9f94a8240a98aa9fde4a7a0fd4f8fd274a881fb
gervais <fseetp@gmail>
2024-06-08 17:26:14 +0200

    cv change size

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,28 +7,34 @@ const NavPortFolio = () => {
   return (
     <>
       <Navbar
+        expand="lg"
         style={{ backgroundColor: "rgba(59, 130, 246, 0.4)" }}
         data-bs-theme="dark"
       >
         <Container>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" style={{ color: "black" }}>
-              {" "}
-              Présentation
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Formation" style={{ color: "black" }}>
-              Formation
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Diplomes" style={{ color: "black" }}>
-              Diplômes
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Projets" style={{ color: "black" }}>
-              Projets
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Curriculum" style={{ color: "black" }}>
-              Curriculum
-            </Nav.Link>
-          </Nav>
+          <Navbar.Brand as={Link} to="/" style={{ color: "black" }}>
+            Portfolio
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/" style={{ color: "black" }}>
+                Présentation
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Formation" style={{ color: "black" }}>
+                Formation
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Diplomes" style={{ color: "black" }}>
+                Diplômes
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Projets" style={{ color: "black" }}>
+                Projets
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Curriculum" style={{ color: "black" }}>
+                Curriculum
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
--- src/Pages/Curriculum.jsx
+++ src/Pages/Curriculum.jsx
@@ -1,14 +1,35 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
 export default function Curriculum() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleImageClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <>
       <h1>Curriculum vitae</h1>
       <Container>
         <div className="curriculum">
-          <Card style={{ width: "70rem" }}>
-            <Card.Img src="/cv/cv-dev.webp" />
+          <Card
+            style={{
+              width: isExpanded ? "auto" : "100%",
+              maxWidth: "70rem",
+              cursor: "pointer",
+            }}
+            onClick={handleImageClick}
+          >
+            <Card.Img
+              src="/cv/cv-dev.webp"
+              style={{
+                width: "100%",
+                height: "auto",
+                objectFit: isExpanded ? "contain" : "cover",
+              }}
+            />
           </Card>
         </div>
       </Container>
